c9f8bb1063526e04af0cfbaa4bfa452baf82e92b
rob <rob@partidosurrealista.com>
2015-03-24 00:57:12 -0600

    sigh, rack

--- helpers/facebook-inbound.rb
+++ helpers/facebook-inbound.rb
@@ -2,11 +2,11 @@ module Event
   module Facebook
 
     def self.refresh_token
-      auth = Koala::Facebook::OAuth.new API::Config.facebook_id, API::Config.facebook_secret
-      nuevo = auth.exchange_access_token(API::Config.access_token)
+      auth = Koala::Facebook::OAuth.new Api::Config.facebook_id, Api::Config.facebook_secret
+      nuevo = auth.exchange_access_token(Api::Config.access_token)
       API.set :facebook, nuevo
-      API::Config.facebook_secret= nuevo
-      API::Config.save
+      Api::Config.facebook_secret= nuevo
+      Api::Config.save
     end
 
     def self.process json, args
--- tasks/facebook.rb
+++ tasks/facebook.rb
@@ -3,13 +3,13 @@ namespace :facebook do
   desc "Suscribirse a notificaciones"
   task :subscribe, [:event, :endpoint] do |task, args|
     updates = Koala::Facebook::RealtimeUpdates.new({
-      app_id: API::Config.facebook_id,
-      secret: API::Config.facebook_secret
+      app_id: Api::Config.facebook_id,
+      secret: Api::Config.facebook_secret
     })
 
 
     begin
-      res = updates.subscribe "user", args[:event].split.join(','), args[:endpoint], API::Config.facebook_verify
+      res = updates.subscribe "user", args[:event].split.join(','), args[:endpoint], Api::Config.facebook_verify
     rescue Koala::Facebook::ClientError => e
       puts "ERROR"
       puts e.message
@@ -23,8 +23,8 @@ namespace :facebook do
   desc "Desuscribirse a notificaciones"
   task :unsubscribe, [:event, :endpoint] do |task, args|
     updates = Koala::Facebook::RealtimeUpdates.new({
-      app_id: API::Config.facebook_id,
-      secret: API::Config.facebook_secret
+      app_id: Api::Config.facebook_id,
+      secret: Api::Config.facebook_secret
     })
 
     begin
