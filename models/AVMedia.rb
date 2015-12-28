1ab41a50d621415bcd28b5d7667b3d5825a508bb
rob <rob@partidosurrealista.com>
2015-12-28 00:52:49 -0600

    implementando realtime de instagram

--- models/AVMedia.rb
+++ models/AVMedia.rb
@@ -31,6 +31,59 @@ class Media
 
   class << self
 
+    def from_instagram item
+      exists = where("meta.id" => item.id)
+      if exists.count > 0
+        m = exists.first
+        m.meta.likes = item.likes.count
+        m.caption = item.caption.text if item.caption
+        m.save
+        return m
+      end
+      size = /(s\d+x\d+)/
+      extra = /(([a-z]?\d+\.){3}\d+)/
+      size_exp = %r!/#{size}!
+      exp = %r!/(#{size}|#{extra})!
+
+      data = {
+        time: Time.at(item.created_time.to_i),
+        url: item.link,
+        type: item.type,
+        meta: {
+          id: item.id,
+          likes: item.likes.count,
+          service: 'instagram'
+        }
+      }
+
+      data[:caption] = item.caption.text if item.caption
+
+      if item.location
+        data[:location] = {
+          type: 'Point',
+          coordinates: [
+            item.location.longitude,
+            item.location.latitude
+          ]
+        }
+      end
+
+      imgs = item.images
+      data[:sizes] = {
+        square: imgs.thumbnail.url.gsub(size_exp, ''),
+        original: imgs.thumbnail.url.gsub(exp, '')
+      }
+
+      if item.videos
+        data[:sizes][:video] = {
+          low: item.videos.low_bandwidth.url,
+          standard: item.videos.standard_resolution.url
+        }
+      end
+
+      Media.create(data)
+    end
+
     def from_service service
       where({"meta.service" => service.to_s})
     end
