13c9894c1c105817628dd800e86c00f0387a9258
rob <rob@partidosurrealista.com>
2015-12-27 22:24:30 -0600

    implementando instagram

--- models/AVMedia.rb
+++ models/AVMedia.rb
@@ -6,24 +6,39 @@ class AVMedia
   field :time, type: Time, default: -> { Time.now }
 
   embeds_many :media, store_as: :media
-
 end
 
 
-class Photo
+class Media
   include Mongoid::Document
 
   field :url, type: String
   field :caption, type: String
   field :time, type: Time, default: -> { Time.now }
+  field :type, type: Symbol, default: :image
 
   field :orientation, type: Symbol
   field :location
 
   field :sizes, type: Hash
+  embeds_one :meta, as: :metadateable
 
   belongs_to :postcard
 
   index location: '2d'
   index({url: 1}, {unique: true, sparse: true})
+
+
+  class << self
+
+    def from_service service
+      where({"meta.service" => service.to_s})
+    end
+
+  end
+end
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural(/^(media)$/i, 'media')
+  inflect.singular(/^(media)$/i, 'media')
 end
--- models/postal.rb
+++ models/postal.rb
@@ -14,15 +14,3 @@ class Postcard
 
   index location: '2d'
 end
-
-class Meta
-  include Mongoid::Document
-  include Mongoid::Attributes::Dynamic
-
-  embedded_in :metadatateable, polymorphic: true
-end
-
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.plural(/^(meta)$/i, 'meta')
-  inflect.singular(/^(meta)$/i, 'meta')
-end
